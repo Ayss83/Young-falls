eeb16cc09ff0657d5fffe5dd84303496ad373f26
Renaud <renoruns@gmail.com>
2017-11-09 17:09:07 +0100

    did a bit of code refactoring

---
+++ services/passport.js
@@ -0,0 +1,17 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+
+//Méthode d'authentification en utilisant le module passport js et google+ api
+passport.use(new GoogleStrategy(
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+        console.log('access token', accessToken);
+        console.log('refresh token', refreshToken);
+        console.log('profile', profile);
+    })
+);
