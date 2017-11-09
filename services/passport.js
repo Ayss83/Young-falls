780bf05749e2d18baf5d31249a089acacd6df5bc
Renaud <renoruns@gmail.com>
2017-11-09 19:03:03 +0100

    Added connection to mongoDB with mongoose and made a user schema to use

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
 
 //Méthode d'authentification en utilisant le module passport js et google+ api
 passport.use(new GoogleStrategy(
@@ -10,8 +13,6 @@ passport.use(new GoogleStrategy(
         callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile', profile);
+        new User({googleId: profile.id}).save();
     })
 );
