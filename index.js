a9698fef79ce2c9acb06919f1ae50554b62ec59f
Renaud <renoruns@gmail.com>
2017-11-09 17:08:04 +0100

    added passport js to authenticate using google

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
+require('./services/passport');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hello: 'world' });
-});
-
+//auto exécution de la fonction retournée
+require('./routes/authRoutes')(app);
 
 //choix dynamique du port d'écoute selon l'environnement d'execution
 const PORT = process.env.PORT || 5000;
